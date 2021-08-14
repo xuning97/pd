dfa163f3657535d0abf22985ecb3d870665b39ec
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2021-08-14 17:03:58 +0800

    scheduler: simplify the limit judgment of hot-region-scheduler (#3834) (#3855)
    Signed-off-by: HunDunDM <hundundm@gmail.com>
    
    Co-authored-by: HunDunDM <hundundm@gmail.com>

--- server/schedulers/scheduler_test.go
+++ server/schedulers/scheduler_test.go
@@ -219,7 +219,7 @@ func (s *testHotRegionSchedulerSuite) TestAbnormalReplica(c *C) {
 	defer cancel()
 	opt := config.NewTestOptions()
 	tc := mockcluster.NewCluster(opt)
-	tc.SetLeaderScheduleLimit(0)
+	tc.SetHotRegionScheduleLimit(0)
 	hb, err := schedule.CreateScheduler(HotReadRegionType, schedule.NewOperatorController(ctx, nil, nil), core.NewStorage(kv.NewMemoryKV()), nil)
 	c.Assert(err, IsNil)
 
@@ -237,7 +237,7 @@ func (s *testHotRegionSchedulerSuite) TestAbnormalReplica(c *C) {
 	tc.AddLeaderRegionWithReadInfo(3, 1, 512*KB*statistics.RegionHeartBeatReportInterval, 0, statistics.RegionHeartBeatReportInterval, []uint64{2, 3})
 	tc.SetHotRegionCacheHitsThreshold(0)
 	c.Assert(tc.IsRegionHot(tc.GetRegion(1)), IsTrue)
-	c.Assert(hb.Schedule(tc), IsNil)
+	c.Assert(hb.IsScheduleAllowed(tc), IsFalse)
 }
 
 var _ = Suite(&testEvictLeaderSuite{})
