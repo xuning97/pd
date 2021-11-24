b2c3de658db98ae2c9a01d341a99697ebca921d6
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2021-11-24 21:43:51 +0800

    schedulers: evict-leader supports schedule the regions with unhealthy peers (#4096) (#4133)
    close #4093
    
    Signed-off-by: HunDunDM <hundundm@gmail.com>
    
    Co-authored-by: HunDunDM <hundundm@gmail.com>

--- server/schedulers/evict_leader.go
+++ server/schedulers/evict_leader.go
@@ -246,12 +246,26 @@ func (s *evictLeaderScheduler) IsScheduleAllowed(cluster opt.Cluster) bool {
 func (s *evictLeaderScheduler) scheduleOnce(cluster opt.Cluster) []*operator.Operator {
 	var ops []*operator.Operator
 	for id, ranges := range s.conf.StoreIDWithRanges {
+		var filters []filter.Filter
 		region := cluster.RandLeaderRegion(id, ranges, opt.HealthRegion(cluster))
 		if region == nil {
-			schedulerCounter.WithLabelValues(s.GetName(), "no-leader").Inc()
-			continue
+			// try to pick unhealthy region
+			region = cluster.RandLeaderRegion(id, ranges)
+			if region == nil {
+				schedulerCounter.WithLabelValues(s.GetName(), "no-leader").Inc()
+				continue
+			}
+			schedulerCounter.WithLabelValues(s.GetName(), "pick-unhealthy-region").Inc()
+			unhealthyPeerStores := make(map[uint64]struct{})
+			for _, peer := range region.GetDownPeers() {
+				unhealthyPeerStores[peer.GetPeer().GetStoreId()] = struct{}{}
+			}
+			for _, peer := range region.GetPendingPeers() {
+				unhealthyPeerStores[peer.GetStoreId()] = struct{}{}
+			}
+			filters = append(filters, filter.NewExcludedFilter(s.GetName(), nil, unhealthyPeerStores))
 		}
-		target := s.selector.SelectTarget(cluster, cluster.GetFollowerStores(region))
+		target := s.selector.SelectTarget(cluster, cluster.GetFollowerStores(region), filters...)
 		if target == nil {
 			schedulerCounter.WithLabelValues(s.GetName(), "no-target-store").Inc()
 			continue
